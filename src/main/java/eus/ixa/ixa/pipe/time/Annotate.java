2efc23292d5e9ad04f758aaeebdc75c0561c303a
cinthyactm <cinthyactm@gmail.com>
2018-07-05 15:33:18 +0200

    Add files via upload

--- src/main/java/eus/ixa/ixa/pipe/time/Annotate.java
+++ src/main/java/eus/ixa/ixa/pipe/time/Annotate.java
@@ -24,11 +24,25 @@ import eus.ixa.ixa.pipe.ml.StatisticalSequenceLabeler;
 import eus.ixa.ixa.pipe.ml.sequence.SequenceLabel;
 import eus.ixa.ixa.pipe.ml.sequence.SequenceLabelerME;
 import eus.ixa.ixa.pipe.ml.utils.Span;
+import info.bethard.timenorm.Temporal;
 import ixa.kaflib.KAFDocument;
 import ixa.kaflib.Timex3;
 import ixa.kaflib.WF;
 
-/**
+import info.bethard.timenorm.TemporalExpressionParser;
+import info.bethard.timenorm.TimeSpan;
+//import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.input.SAXBuilder;
+
+
+
+
+import scala.util.Try;
+        /**
  * Annotation class for Named Entities in ixa-pipe-time. Use this class for
  * examples on using ixa-pipe-ml API for Named Entity tagging.
  * 
@@ -55,9 +69,12 @@ public class Annotate {
 
   public final void annotateTimeToKAF(final KAFDocument kaf)
       throws IOException {
-
+ //   String  creationTime=kaf.createTimestamp();
+    //  LocalDate DateTime = LocalDate.parse("2018-06-26");//, DateFormatter..ofPattern("yyyy-mm-dd"));
+    LocalDateTime DateTime = LocalDateTime.parse(kaf.createTimestamp(), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ"));
+ 
     List<List<WF>> sentences = kaf.getSentences();
-
+    TemporalExpressionParser parserT = TemporalExpressionParser.en();
     for (List<WF> sentence : sentences) {
       // process each sentence
       String[] tokens = new String[sentence.size()];
@@ -72,13 +89,19 @@ public class Annotate {
       Span[] allSpansArray = SequenceLabelerME.dropOverlappingSpans(statSpans);
       List<SequenceLabel> names = temporalTagger.getSequencesFromSpans(tokens,
           allSpansArray);
-      for (SequenceLabel name : names) {
+     for (SequenceLabel name : names) {
         Integer startIndex = name.getSpan().getStart();
         Integer endIndex = name.getSpan().getEnd();
         List<WF> nameWFs = sentence.subList(startIndex, endIndex);
         ixa.kaflib.Span<WF> neSpan = KAFDocument.newWFSpan(nameWFs);
         Timex3 timex3 = kaf.newTimex3(name.getType());
+        Try<Temporal> temp = parserT.parse(name.getString(), TimeSpan.of(DateTime.getYear(),DateTime.getMonthValue(), DateTime.getDayOfMonth()));// "2018-06-26T12:35:47+0200");
+         timex3.setValue(temp.get().timeMLValue());
+     
+                 
         timex3.setSpan(neSpan);
+     
+     
       }
       if (clearFeatures.equalsIgnoreCase("yes")) {
         temporalTagger.clearAdaptiveData();
@@ -90,8 +113,9 @@ public class Annotate {
   }
 
   public String annotateToTimeML(KAFDocument kaf) {
-
+      
     return kaf.toString();
   }
 
 }
+
